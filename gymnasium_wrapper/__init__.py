4e84fd28a0430ff0bce959b2b9fc8ff72335a746
Hugo <hugo@hugohuang.com>
2026-01-08 04:18:18 +0000

    Correct typos in gymnasium wrapper registration code

--- gymnasium_wrapper/__init__.py
+++ gymnasium_wrapper/__init__.py
@@ -260,8 +260,8 @@ for skill in SKILL_LEVELS:
                 kwargs={
                     "scenario_config_file": f"{config_file}",
                     "additional_config_dict": {
-                        "skill_level": skill_int,
-                        "map": map,
+                        "doom_skill": skill_int,
+                        "doom_map": map,
                     },
                     "max_buttons_pressed": 0,
                 },
